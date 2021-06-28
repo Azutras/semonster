abbcf1f085f3c17b5887fa3643d48d9f025d6892
Azutras <coppsz3@outlook.jp>
2021-06-28 14:32:17 +0900

    merge commit fixed

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
   public static void main(String[] args) {
-    System.out.println("S#MONSTER!");
+    System.out.println("SEMONSTER!");
     Monster monster = new Monster();
     System.out.println(monster.toString());
     Player player = new Player();
